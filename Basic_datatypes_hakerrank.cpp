b13ff1663d196d0ffd31faa13dc579fa506d384d
AmmarMohamed <ammarabdelghany30@gmail.com>
2025-06-21 21:30:42 +0300

    First problem is done

---
+++ Basic_datatypes_hakerrank.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+
+    scanf("%d %lld %c %f %lf", &a, &b, &c, &d,&e);
+  //  printf("%d %ld %c %f %lf %lf\n", a, b, c, d, e);
+    printf("%d\n",a);
+    printf("%lld\n",b);
+    printf("%c\n",c);
+    printf("%f\n",d);
+    printf("%lf\n",e);
+    return 0;
+
+}
