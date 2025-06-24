f15fca599432797ece4eb1e74642e56930c86f3f
AmmarMohamed <ammarabdelghany30@gmail.com>
2025-06-24 07:28:31 +0300

    Stairs problem solution

--- Week 1/Dreamoon_and_stairs.cpp
+++ Week 1/Dreamoon_and_stairs.cpp
@@ -2,13 +2,26 @@
 using namespace std;
 int main(){
 	
-	
-	cout<<"hello";
-	
-	
-	
-	
-	
-	
-	
+	int steps,m,min_steps;
+	cin>>steps>>m;
+	const int max_steps=steps;
+	if (steps%2==0) {
+		 min_steps =steps/2;
+	}
+	else {
+		 min_steps =(steps/2)+1;
+	}
+
+
+	for (int i = min_steps;i<=max_steps;i++) {
+		if (i%m==0) {
+			cout<<i<<endl;
+			return 0;
+		}
+
+	}
+
+	cout<<-1<<endl;
+	return 0;
+
 }
